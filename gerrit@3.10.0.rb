63ea5dc79a508e1eea9f653723dd987b2bd7342f
Daniele Sassoli <danielesassoli@gmail.com>
2024-11-29 16:26:24 +0000

    Add support for Gerrit 3.10
    Change-Id: I575f7b3f143701d9c3d9131f6db5230baffba389

---
+++ gerrit@3.10.0.rb
@@ -0,0 +1,18 @@
+# typed: false
+# frozen_string_literal: true
+
+require_relative 'gerrit_base'
+
+class GerritAT3100 < GerritBase
+
+  def self.java_version
+    "17"
+  end
+
+  set_common_dependencies
+
+  version "3.10.0"
+  sha256 "f18c23f6c0911012ec03dbbde09788d4d6d470e977312244c416dd6e0d3b4ae1"
+  url generate_url(version)
+
+end
