74f887354da5e5d372698c3c1f0d2c211d628472
Daniele Sassoli <danielesassoli@gmail.com>
2024-12-05 10:54:15 +0000

    Add plain Gerrit formula
    Change-Id: I6341579a35bc374a93fb7bdc979d0e46fdb75f40

---
+++ gerrit.rb
@@ -0,0 +1,17 @@
+# typed: false
+# frozen_string_literal: true
+
+require_relative "./gerrit-base"
+
+class Gerrit < GerritBase
+  def self.java_version
+    "21"
+  end
+
+  set_common_dependencies
+
+  version "3.11.0"
+  sha256 "f18c23f6c0911012ec03dbbde09788d4d6d470e977312244c416dd6e0d3b4ae1"
+  url generate_url(version)
+
+end
