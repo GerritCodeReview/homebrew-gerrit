3f57defcd4743293595743c973a5189c7817c633
Luca Milanesio <luca.milanesio@gmail.com>
2025-07-17 17:03:25 +0100

    Add Gerrit 3.10.7
    Change-Id: Ie3b3ebe54417b86d629376bce90001c2930a0057

---
+++ gerrit@3.10.7.rb
@@ -0,0 +1,18 @@
+# typed: false
+# frozen_string_literal: true
+
+require_relative 'gerritbase'
+
+class GerritAT3106 < GerritBase
+
+  def self.java_version
+    "17"
+  end
+
+  set_common_dependencies
+
+  version "3.10.7"
+  sha256 "5b89cbd1700431ef802db1a2b498287b45817b40b88d2752415f3087653d597a"
+  url generate_url(version)
+
+end
