bca05621b5096ec4fdc79504ee8541a7afa9b115
Luca Milanesio <luca.milanesio@gmail.com>
2025-07-18 23:38:20 +0100

    Set Gerrit 3.12.1 as latest version
    Change-Id: I732603d0cfb919addf7bceb2b50535311c43ef91

--- gerrit.rb
+++ gerrit.rb
@@ -10,8 +10,8 @@ class Gerrit < GerritBase
 
   set_common_dependencies
 
-  version "3.12.0"
-  sha256 "8a8e55689c91e36ac7717c969c5a05ffd5a1ecdd0ce10bf31572ab127044c5b1"
+  version "3.12.1"
+  sha256 "a106a8bd1dbbff0d119cb53a7e110674cd1106ab6675d3a51584bddb2c537041"
   url generate_url(version)
 
 end
