21eb13a40166b68a80aff39fe16dbdb710ab4142
Luca Milanesio <luca.milanesio@gmail.com>
2025-08-15 00:18:14 +0200

    Add Gerrit 3.12.2
    Change-Id: I21c9d500eb829ff72707ac14ed06c806137f4c33

---
+++ gerrit@3.12.2.rb
@@ -0,0 +1,17 @@
+# typed: false
+# frozen_string_literal: true
+
+require_relative "./gerritbase"
+
+class GerritAT3122 < GerritBase
+  def self.java_version
+    "21"
+  end
+
+  set_common_dependencies
+
+  version "3.12.2"
+  sha256 "18b3bb9e6dd20992e1ff3a83964551ae06843bc45f2539d65a63161ec64f5485"
+  url generate_url(version)
+
+end
