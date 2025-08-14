6714d96df36319a50898ade406ba918b5fb2fa5c
Luca Milanesio <luca.milanesio@gmail.com>
2025-08-15 00:21:43 +0200

    Set Gerrit 3.12.2 as latest version
    Change-Id: I216fcfa22d5fbd224acecfd62789fe099255d414

--- gerrit.rb
+++ gerrit.rb
@@ -3,15 +3,15 @@
 
 require_relative "./gerritbase"
 
-class Gerrit < GerritBase
+class GerritAT3122 < GerritBase
   def self.java_version
     "21"
   end
 
   set_common_dependencies
 
-  version "3.12.1"
-  sha256 "a106a8bd1dbbff0d119cb53a7e110674cd1106ab6675d3a51584bddb2c537041"
+  version "3.12.2"
+  sha256 "18b3bb9e6dd20992e1ff3a83964551ae06843bc45f2539d65a63161ec64f5485"
   url generate_url(version)
 
 end
