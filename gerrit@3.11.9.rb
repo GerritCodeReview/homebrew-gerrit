5b72f05d84f0229e8d6a5b6e1e762f59f69b2abb
Luca Milanesio <luca.milanesio@gmail.com>
2026-03-01 21:52:26 +0000

    Add Gerrit 3.11.9
    Change-Id: Ibbf88a57e95245a96d3aba3a6dcc8f8afdf578d2

---
+++ gerrit@3.11.9.rb
@@ -0,0 +1,17 @@
+# typed: false
+# frozen_string_literal: true
+
+require_relative "./gerritbase"
+
+class GerritAT3119 < GerritBase
+  def self.java_version
+    "21"
+  end
+
+  set_common_dependencies
+
+  version "3.11.9"
+  sha256 "9526cb04f60194d25e4294d55d3582ffeb9ee60ba59a9cdf3473e0c9c296d47a"
+  url generate_url(version)
+
+end
